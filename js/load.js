8db2b582f6ab61f707545828ae213ff391057395
Daniru2007 <daniruayuka@gmail.com>
2022-08-04 18:33:13 +0530

    slight animation for players

---
+++ js/load.js
@@ -0,0 +1,18 @@
+let players = document.querySelectorAll(".right,.left");
+
+function displayPlayers(entries, observer) {
+    entries.forEach((entry) => {
+        elm = entry.target;
+        if (entry.isIntersecting) {
+            elm.classList.add("active");
+        } else {
+            elm.classList.remove("active");
+        }
+    });
+}
+
+let displayObserver = new IntersectionObserver(displayPlayers);
+
+players.forEach((player) => {
+    displayObserver.observe(player);
+});
